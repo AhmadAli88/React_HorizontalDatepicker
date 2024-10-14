197c273b20e79016212f78a6643a5886683ed748
Ahmad Ali <xevenahmad@gmail.com>
2024-10-14 10:48:51 +0500

    finalize react horizontal datepicker

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,17 @@ function App() {
 };
   return (
     <>
+     <div className="datepicker-container">
+     <h2>Select a Date</h2>
       <DatePicker
         getSelectedDay={selectedDay}
         endDate={100}
         selectDate={new Date("2020-04-30")}
         labelFormat={"MMMM"}
-        color={"#374e8c"}
+        color={"lightgreen"}
+      
       />
+      </div>
     </>
   );
 }
